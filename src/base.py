58139132a6877b6a3ae5814655442bb104bed2c5
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-06-29 14:13:08 +1000

    add file

---
+++ src/base.py
@@ -0,0 +1,48 @@
+import argparse
+from datetime import datetime
+
+from selenium import webdriver
+
+
+def make_webdriver():
+    # adding webdriver options
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--profile-directory=Default')
+    options.add_argument('--user-data-dir=~/.config/google-chrome')
+    return webdriver.Chrome(options=options)
+
+
+def convert_datetime(o):
+    # convert datetime format to fit json
+    if isinstance(o, datetime):
+        return o.__str__()
+
+
+def argparse_dtype_converter(a: str) -> bool:
+    """Convert string to boolean
+
+    Args:
+        a (str): Variable to be converted to boolean
+
+    Raises:
+        argparse.ArgumentTypeError: Invalid variable
+
+    Returns:
+        bool: Boolean value
+    """
+    if isinstance(a, bool):
+        return a
+    
+    elif a.lower() in ("yes", "y", "true", "t", "1"):
+        return True
+
+    elif a.lower() in ("no", "n", "false", "f", "0"):
+        return False
+
+    else:
+        raise argparse.ArgumentTypeError(f"Expected boolean value. Got {a}.")
