d9a472b7cb4dffc5ac124436bc05c28bafa64c44
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-07-22 15:27:46 +1000

    typo

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,4 +1,5 @@
 import re
+from typing import Union
 
 import pandas as pd
 from typeguard import typechecked
@@ -14,27 +15,27 @@ def pre_process(data: pd.DataFrame):
 
 
 @typechecked
-def measurement_cleaning(x: str): 
+def measurement_cleaning(x: Union[str, float]): 
 
     if not isinstance(x, str):
         return []
 
     quantity = float(re.match(r"\d+", x).group())
-    measument = re.findall(r"[a-zA-z]+", x)[0]
+    measurement = re.findall(r"[a-zA-z]+", x)[0]
 
-    measuments_dict = {"g": "kg"
+    measurements_dict = {"g": "kg"
                     ,"ml": "l"
                     ,"ea": "each", "each": "each"
                     } 
 
-    if measument.lower() in ["kg", "l"]:
-        return [quantity, measument.lower()]
+    if measurement.lower() in ["kg", "l"]:
+        return [quantity, measurement.lower()]
 
-    elif measument.lower() in ["g", "ml"]:
-        return [quantity/1000, measuments_dict[measument.lower()]]
+    elif measurement.lower() in ["g", "ml"]:
+        return [quantity/1000, measurements_dict[measurement.lower()]]
 
-    elif measument.lower() in ["ea", "each"]:
-        return [quantity, measuments_dict[measument.lower()]]
+    elif measurement.lower() in ["ea", "each"]:
+        return [quantity, measurements_dict[measurement.lower()]]
 
     else:
-        return [quantity, measument]
+        return [quantity, measurement]
