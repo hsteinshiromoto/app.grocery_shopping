0fdd613d01a2e1f06d066ac8f3cd3e802c670579
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-09-08 09:29:14 +1000

    bugfix

--- tests/test_get_prices.py
+++ tests/test_get_prices.py
@@ -1,8 +1,11 @@
 import subprocess
 from pathlib import Path
+import sys
 
 PROJECT_ROOT = Path(subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], 
                                 stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8'))
+
+sys.path.append(str(PROJECT_ROOT))
 
 from src.data.get_prices import Coles, Woolworths
 
