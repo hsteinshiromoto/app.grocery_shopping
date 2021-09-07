7dd468a0a093777a37db56e85bc53babb762050f
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2021-09-07 22:03:34 +1000

    bugfixes

--- tests/test_get_prices.py
+++ tests/test_get_prices.py
@@ -4,7 +4,7 @@ from pathlib import Path
 PROJECT_ROOT = Path(subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], 
                                 stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8'))
 
-from src.get_prices import Coles, Woolworths
+from src.data.get_prices import Coles, Woolworths
 
 def test_coles_url_response(supermarket=Coles(), product_category: str="milk"
                         ,page_number: int=1):
--- tests/test_web_api.py
+++ tests/test_web_api.py
@@ -5,7 +5,7 @@ PROJECT_ROOT = Path(subprocess.Popen(['git', 'rev-parse', '--show-toplevel'],
                                 stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8'))
 DATA = PROJECT_ROOT / "data"
 
-from src.web_api import WebAPI
+from src.api.web import WebAPI
 
 def test_webapi():
     api = WebAPI()
